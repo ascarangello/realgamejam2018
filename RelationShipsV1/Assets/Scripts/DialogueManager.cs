4ec3d7cc52c9eece194734414cd833cc3eccc653
MitchellMcKee <mckee.m@husky.neu.edu>
2018-01-27 13:59:15 -0500

    Prototype 1 but it wors maybe

---
+++ RelationShipsV1/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour {
+
+    public GameObject[] choices;
+    private Queue<string> sentences;
+    public static int index;
+
+	void Start () {
+        sentences = new Queue<string>();
+	}
+
+    public void StartDialogue (Dialogue dialogue)
+    {
+        HideAllChoices();
+
+        Debug.Log("Starting conversation with " + dialogue.name);
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence ()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        Debug.Log(sentence);
+    }
+
+    void EndDialogue()
+    {
+        PresentChoices();
+        Debug.Log("End of conversation");   
+    }
+
+    public void PresentChoices ()
+    {
+        choices[index].SetActive(true);
+        Debug.Log(index);
+    }
+    public void HideAllChoices()
+    {
+        for (int i = 0; i < choices.Length; i++)
+        {
+            choices[i].SetActive(false);
+        }
+    }
+}
