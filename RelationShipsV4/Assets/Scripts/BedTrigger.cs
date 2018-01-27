0c9180ac007901ba15aacc85b573f01273a201e0
thexammer <thexammer@gmail.com>
2018-01-27 18:34:51 -0500

    prototype for Jock conversation

---
+++ RelationShipsV4/Assets/Scripts/BedTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BedTrigger : MonoBehaviour {
+
+    private System.Timers.Timer timer = new System.Timers.Timer();
+
+    private void OnTriggerEnter2D(Collider2D collidedObject)
+    {
+        Debug.Log("YOU SLEEP");
+        timer.Elapsed += OnTimer;
+        timer.Interval = 2000;
+        timer.AutoReset = false;
+        timer.Enabled = true;
+        PlayerController.sleeping = true;
+        collidedObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+        collidedObject.GetComponent<Transform>().position = GetComponent<Transform>().position;
+    }
+
+    private void OnTimer(object source, System.Timers.ElapsedEventArgs e)
+    {
+        PlayerController.sleeping = false;
+
+    }
+}
---
+++ RelationShipsV4/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody2D rb;
+    public float speed;
+    public static bool sleeping = false;
+    
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	void FixedUpdate () {
+        float xMove = Input.GetAxis("Horizontal");
+        float yMove = Input.GetAxis("Vertical");
+        if (!sleeping)
+        {
+            rb.velocity = (new Vector2(xMove * speed, yMove * speed));
+        }
+	}
+
+}
