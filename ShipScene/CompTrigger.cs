ad6f80f37b75a30dbbb13f92504f82eda9522d91
thexammer <thexammer@gmail.com>
2018-01-27 14:48:11 -0500

    here's the ship my dudes

---
+++ ShipScene/CompTrigger.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompTrigger : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D collidedObject)
+    {
+        Debug.Log("YOU HIT COMPUTER BEEP BORP");
+    }
+}
