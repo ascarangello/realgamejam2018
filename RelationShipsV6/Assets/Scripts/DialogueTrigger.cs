918356eef9a6fb609c36fd0364115d32a9b257e1
MitchellMcKee <mckee.m@husky.neu.edu>
2018-01-27 21:31:00 -0500

    with template for dialogue

---
+++ RelationShipsV6/Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour {
+
+    public Dialogue dialogue;
+    public int jumpToTree;
+
+    public int jockAffection;
+    public int nerdAffection;
+    public int prepAffection;
+    public int gothAffection;
+
+    public void TriggerDialogue ()  {
+
+        GameManager.nerdLove = GameManager.nerdLove+nerdAffection;
+        GameManager.jockLove = GameManager.jockLove + jockAffection;
+        GameManager.prepLove = GameManager.prepLove + prepAffection;
+        GameManager.gothLove = GameManager.gothLove + gothAffection;
+        DialogueManager.index = jumpToTree;
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+
+}
