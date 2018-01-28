328c39f195e934c89e6610cb212fd1a9e66774a0
MitchellMcKee <mckee.m@husky.neu.edu>
2018-01-27 20:14:06 -0500

    Only assets

---
+++ AssetsofV5/Scripts/CompTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompTrigger : MonoBehaviour {
+
+    public GameManager scenes;
+
+    private void OnTriggerEnter2D(Collider2D collidedObject)
+    {
+        GameManager.playerByComp = true;
+        scenes.LoadDesktop();
+    }
+}
---
+++ AssetsofV5/Scripts/DesktopControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DesktopControl : MonoBehaviour {
+
+    public GameManager scenes;
+
+    public void ExitDesktop()
+    {
+        FindObjectOfType<AudioManager>().Play("Click");
+        scenes.LoadShip();
+    }
+
+    public void OpenTestChat()
+    {
+        FindObjectOfType<AudioManager>().Play("Click");
+        scenes.LoadNerd();
+    }
+
+    public void BackToDesktop()
+    {
+        FindObjectOfType<AudioManager>().Play("Click");
+        scenes.LoadDesktop();
+    }
+
+}
