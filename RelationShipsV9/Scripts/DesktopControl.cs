367875f0bfbe4a7de2961bfe829ab6b173191f74
MitchellMcKee <mckee.m@husky.neu.edu>
2018-01-28 15:25:46 -0500

    film this

---
+++ RelationShipsV9/Scripts/DesktopControl.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DesktopControl : MonoBehaviour {
+
+    public GameManager scenes;
+
+    public void ExitDesktop()
+    {
+        FindObjectOfType<AudioManager>().Play("Click");
+        scenes.LoadShip();
+    }
+
+    public void BackToDesktop()
+    {
+        FindObjectOfType<AudioManager>().Play("Click");
+        scenes.LoadShip();
+    }
+
+    public void edgyPath()
+    {
+        FindObjectOfType<AudioManager>().Play("Click");
+        scenes.LoadGoth1();
+    }
+
+    public void prepPath()
+    {
+        FindObjectOfType<AudioManager>().Play("Click");
+        scenes.LoadPrep1();
+    }
+
+    public void jockPath()
+    {
+        FindObjectOfType<AudioManager>().Play("Click");
+        scenes.LoadJock1();
+    }
+    public void nerdPath()
+    {
+        FindObjectOfType<AudioManager>().Play("Click");
+        scenes.LoadNerd1();
+    }
+}
---
+++ RelationShipsV9/Scripts/GameManager.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    public static bool playerByComp;
+    public static bool firstComp;
+    public static bool postPreGC;
+
+    // 1 = goth, 2 = prep, 3 = jock, 4 = nerd
+    public static int path;
+
+    public static int day;    
+
+    public static int nerdLove;
+    public static int jockLove;
+    public static int prepLove;
+    public static int gothLove;
+
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void Start()
+    {
+        nerdLove = 0;
+        jockLove = 0;
+        prepLove = 0;
+        gothLove = 0;
+
+        firstComp = true;
+        postPreGC = false;
+        day = 0;
+    }
+
+    public void LoadShip()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadPreBranchGC()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void LoadDesktop()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    // Edgy path
+    public void LoadGoth1()
+    {
+        path = 1;
+        SceneManager.LoadScene(7);
+    }
+    public void LoadGoth2()
+    {
+        day += 1;
+        SceneManager.LoadScene(8);
+    }
+    public void LoadGoth3()
+    {
+        SceneManager.LoadScene(9);
+    }
+
+    // Prep path
+    public void LoadPrep1()
+    {
+        path = 2;
+        SceneManager.LoadScene(4);
+    }
+    public void LoadPrep2()
+    {
+        day += 1;
+        SceneManager.LoadScene(5);
+    }
+    public void LoadPrep3()
+    {
+        SceneManager.LoadScene(6);
+    }
+
+    // Jock path
+    public void LoadJock1()
+    {
+        path = 3;
+        SceneManager.LoadScene(10);
+    }
+    public void LoadJock2()
+    {
+        day += 1;
+        SceneManager.LoadScene(11);
+    }
+    public void LoadJock3()
+    {
+        SceneManager.LoadScene(12);
+    }
+
+    // Nerd path
+    public void LoadNerd1()
+    {
+        path = 4;
+        SceneManager.LoadScene(13);
+    }
+    public void LoadNerd2()
+    {
+        day += 1;
+        SceneManager.LoadScene(14);
+    }
+    public void LoadNerd3()
+    {
+        SceneManager.LoadScene(15);
+    }
+
+}
